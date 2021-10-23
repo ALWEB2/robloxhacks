c7df537d93ac19fdff18507af6f4b37e79155589
Unblocked-x Host <92831976+ALWEB2@users.noreply.github.com>
2021-10-23 17:35:37 -0400

    a
    a

--- FULL SCRIPT.lua
+++
@@ -1,5 +0,0 @@
--- Made by JerryToaster
--- godmode loop
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ALWEB2/robloxhacks/main/loopgod.lua'))()
--- super killaura
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ALWEB2/robloxhacks/main/swordfight.lua'))()
