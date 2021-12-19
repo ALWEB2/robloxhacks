e9b646e7a3952668ecfef1cf5eddb8d9157d5533
Unblocked-x Host <92831976+ALWEB2@users.noreply.github.com>
2021-12-18 20:54:35 -0500

    new version
    a

--- anomicultimate.lua
+++ anomicultimate.lua
@@ -1,4 +1,11 @@
 -- made by jerrytoaster556 for mre
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Main%20Bypass.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Pick%20All%20UnLocked%20Printers.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Pick%20all%20dropped%20tools.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Unlock%20and%20Cash%20Printers.lua'))()
+
+--fixed bugs
+wait(2)
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/colbmane/leaked-shit/main/yoderwork'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Anti%20Anti%20Teleport%20But%20Less%20Taxing.lua'))()
@@ -8,11 +15,7 @@ loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Baton%20Farm%20Script.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Inf%20Stamina.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Instant%20Jail%20%2B%20Baton%20Destroy%20Printer%20%2B%20one%20more%20ok%20thing.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Main%20Bypass.lua'))()
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Name%20Who.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/OP%20gun%20support%20script.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/OP%20shotgun%20script.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Pick%20All%20UnLocked%20Printers.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Pick%20all%20dropped%20tools.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/Unlock%20and%20Cash%20Printers.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Shariiii/Anomic-Stuff/main/WalkSpeed%20Bypass%20(Open%20Source).lua'))()
